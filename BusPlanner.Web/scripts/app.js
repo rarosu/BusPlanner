0ac1f681798c1fbeb7324d4368421b4a37a4043f
Lars Woxberg <lars.woxberg@gmail.com>
2015-11-10 10:14:02 +0100

    Migrated client dependencies to bower
    Due to convention, bower is used for front-end, even though npm3 has
    support for flat dependency trees. Also restructured the angular project
    and added an initial view.

---
+++ BusPlanner.Web/scripts/app.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    "use strict";
+
+    var app = angular.module('app', [
+        'ngRoute'
+    ]);
+
+    app.config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when('/index', {
+                templateUrl: 'views/Home.html',
+                controller: 'HomeController'
+            })
+            .otherwise({
+                redirectTo: '/index'
+            });
+    }]);
+})();
---
+++ BusPlanner.Web/scripts/controllers/HomeController.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    "use strict";
+
+    angular
+        .module('app')
+        .controller('HomeController', function () {
+            var vm = this;
+
+            vm.testdata = [ 
+                { 
+                    name: "Hello",
+                    description: "Description"
+                },
+                {
+                    name: "Bye",
+                    description: "No description"
+                }
+            ];
+        });
+})();
