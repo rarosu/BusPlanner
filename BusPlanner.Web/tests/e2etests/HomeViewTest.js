b10fe401018ee0549bedf1aa45f19c8081084ccc
Lars Woxberg <lars.woxberg@gmail.com>
2015-11-10 13:25:46 +0100

    Setting up protractor end to end testing.
    Everything is not sorted out yet.

---
+++ BusPlanner.Web/tests/e2etests/HomeViewTest.js
@@ -0,0 +1,8 @@
+﻿describe('Home page', function () {
+    it('should redirect index.html to index.html#/index', function () {
+        browser.get('index.html');
+        browser.getLocationAbsUrl().then(function (url) {
+            expect(url).toEqual('/index');
+        });
+    });
+});
