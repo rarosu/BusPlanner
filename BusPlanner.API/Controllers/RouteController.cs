ce5786ad44cee5c8688c379c7a8db360ce44d06a
Lars Woxberg <lars.woxberg@gmail.com>
2015-11-11 16:47:03 +0100

    Set up a bare Web API project.
    api/route can now be accessed and JSON is returned. Everything in the
    backend is hard-coded.

---
+++ BusPlanner.API/Controllers/RouteController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace BusPlanner.API.Controllers
+{
+    public class Bus
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Bus PartOfBus { get; set; }
+    }
+
+    public class RouteController : ApiController
+    {
+        [Route("api/route")]
+        public Bus Get()
+        {
+            Bus b = new Bus();
+            b.Id = 0;
+            b.Name = "Hello";
+            b.PartOfBus = new Bus();
+            b.PartOfBus.Id = 1;
+            b.PartOfBus.Name = "World";
+
+            return b;
+
+            //return "{ name: \"hello\", description: \"From API\" }";
+        }
+    }
+}
---
+++ BusPlanner.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BusPlanner.API.Global" Language="C#" %>
