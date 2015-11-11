08d0929ea7c15a6e67268ab01d700df7d6d8a9c5
Lars Woxberg <lars.woxberg@gmail.com>
2015-11-11 14:12:00 +0100

    Made standalone e2e tests working.
    Need to have a web server serving the root at port 8080. Then run
    'protractor protractor.conf.js'. Will work on a gulp task for this menial
    work.

--- BusPlanner.Web/protractor.conf.js
+++ BusPlanner.Web/protractor.conf.js
@@ -1,11 +1,25 @@
+// An example configuration file.
 exports.config = {
-	seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: [
-        'tests/e2etests/*.js'
-    ],
-    capabilities: {
-        'browserName': 'firefox'
-    },
-    baseUrl: 'http://localhost:8000/',
-    framework: 'jasmine'
+	directConnect: true,
+
+	// Capabilities to be passed to the webdriver instance.
+	capabilities: {
+		'browserName': 'chrome'
+	},
+
+	// Framework to use. Jasmine 2 is recommended.
+	framework: 'jasmine2',
+
+	// Spec patterns are relative to the current working directly when
+	// protractor is called.
+	specs: ['tests/e2etests/**/*.js'],
+	
+	// Chrome only standalone
+	chromeDriver: './node_modules/protractor/selenium/chromedriver',
+	chromeOnly: true,
+
+	// Options to be passed to Jasmine.
+	jasmineNodeOpts: {
+		defaultTimeoutInterval: 30000
+	}
 };
--- BusPlanner.Web/tests/e2etests/HomeViewTest.js
+++ BusPlanner.Web/tests/e2etests/HomeViewTest.js
@@ -1,8 +1,12 @@
 ﻿describe('Home page', function () {
+	beforeEach(function() {
+		
+	});
+	
     it('should redirect index.html to index.html#/index', function () {
-        browser.get('index.html');
-        browser.getLocationAbsUrl().then(function (url) {
-            expect(url).toEqual('/index');
-        });
+        browser.get('http://localhost:8080');
+		browser.getLocationAbsUrl().then(function(url) {
+			expect(url).toEqual('/index');
+		});
     });
 });
