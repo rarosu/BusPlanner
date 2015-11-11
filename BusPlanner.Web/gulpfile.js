280cc490892b3e9016278872cc977695693610b9
Lars Woxberg <lars.woxberg@gmail.com>
2015-11-11 14:31:28 +0100

    Gulp task for running e2e tests implemented.
    Can not be run since gulp-protractor will fail if any of the files or
    directories for the specs have spaces in them. This appears to be an
    upstream problem in child_process.spawn. Will have to move the project to
    a directory without spaces.

--- BusPlanner.Web/gulpfile.js
+++ BusPlanner.Web/gulpfile.js
@@ -1,10 +1,24 @@
 ﻿var gulp = require('gulp');
 var karma = require('karma');
+var connect = require('gulp-connect');
+var protractor = require('gulp-protractor').protractor;
 
 gulp.task('tdd', function(done) {
 	new karma.Server({
 		configFile: __dirname + '/karma.conf.js'
 	}, done).start();
+});
+
+gulp.task('e2e', function() {
+	connect.server();
+	
+	gulp.src(["./tests/e2etests/**/*.js"])
+		.pipe(protractor({
+			configFile: './protractor.conf.js'
+		}))
+		.on('error', function(e) {
+			throw e;
+		});
 });
 
 gulp.task('default', ['tdd']);
