1085b0f8aa596f3b10a1ab92ddc383790ca16025
Lars Woxberg <lars.woxberg@gmail.com>
2015-11-17 09:01:02 +0100

    Created a script for migrating.
    Also created a config file for the connection strings. Made a project for
    integration tests to just place a config file for the connection strings
    for now.

---
+++ BusPlanner.IntegrationTests/StopRepositoryTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BusPlanner.IntegrationTests
+{
+    [TestClass]
+    public class StopRepositoryTests
+    {
+        [TestMethod]
+        public void TestDbConnection()
+        {
+        }
+    }
+}
