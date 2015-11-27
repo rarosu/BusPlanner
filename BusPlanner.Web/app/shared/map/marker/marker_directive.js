e0a90b8793e71485a5b0e07af9aa6ff55c2768b7
Lars Woxberg <lars.woxberg@gmail.com>
2015-11-27 16:25:14 +0100

    Added marker directive to the map module.

---
+++ BusPlanner.Web/app/shared/map/marker/marker_directive.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    "use strict";
+
+    angular.module('map')
+    .directive('googleMapMarker', [function () {
+        return {
+            restrict: 'E',
+            transclude: true,
+            templateUrl: 'shared/map/marker/marker_template.html',
+            scope: {
+                position: '=',
+                title: '@'
+            },
+            require: '^googleMap',
+            link: function (scope, element, attrs, controller) {
+                controller.mapPromise.then(function (map) {
+                    console.log("marker: map has been created");
+                    console.log(map);
+
+                    var marker = new google.maps.Marker({
+                        position: scope.position,
+                        map: map,
+                        title: scope.title
+                    });
+                });
+            }
+        };
+    }]);
+})();
