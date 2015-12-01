0d344ba9774adfb67f6eb30433c51e15a9badae2
Lars Woxberg <lars.woxberg@gmail.com>
2015-12-01 14:26:41 +0100

    Map directives modify transcluded scope.
    Map directives can now expose their gmap elements to child elements in a
    flexible way. The context menu has not been updated.

--- BusPlanner.Web/app/shared/map/map_directive.js
+++ BusPlanner.Web/app/shared/map/map_directive.js
@@ -2,33 +2,41 @@
     "use strict";
 
     angular.module('map')
-    .directive('googleMap', ['mapLoader', function (mapLoader) {
+    .directive('googleMap', ['$q', 'mapLoader', function ($q, mapLoader) {
         return {
             restrict: 'E',
             transclude: true,
             templateUrl: 'shared/map/map_template.html',
             scope: {
+                gmapElement: '@',
                 containerId: '@',
                 center: '=',
                 zoom: '='
             },
-            controller: ['$q', function ($q) {
-                this._deferred = $q.defer();
-                this.ready = function (callback) {
-                    this._deferred.promise.then(callback);
-                };
-            }],
-            link: function (scope, element, attrs, controller) {
+            link: function (scope, element, attrs, controller, transcludeFn) {
+                // Create a new transclusion scope and add a map promise to it.
+                var transclusionScope = scope.$new();
+                var transclusionTarget = element[0].querySelector('[transclude-target]');
+                transclusionScope[scope.gmapElement] = $q.defer();
+                transcludeFn(transclusionScope, function (clone) {
+                    angular.element(transclusionTarget).append(clone);
+                });
+
                 mapLoader.then(function (maps) {
+                    // Create a map element.
                     var mapElement = element.children()[0];
                     var map = new google.maps.Map(mapElement, {
                         center: scope.center,
                         zoom: scope.zoom
                     });
 
+                    // An idle event is received when the map is ready for use.
                     google.maps.event.addListenerOnce(map, 'idle', function () {
+                        // Trigger a resize to make sure that new instances of the map are not rendered grey.
                         google.maps.event.trigger(map, 'resize');
-                        controller._deferred.resolve(map);
+
+                        // The map is ready for use.
+                        transclusionScope[scope.gmapElement].resolve(map);
                     });
                 });
             }
--- BusPlanner.Web/app/shared/map/marker/marker_directive.js
+++ BusPlanner.Web/app/shared/map/marker/marker_directive.js
@@ -2,33 +2,35 @@
     "use strict";
 
     angular.module('map')
-    .directive('googleMapMarker', [function () {
+    .directive('googleMapMarker', ['$q', function ($q) {
         return {
             restrict: 'E',
             transclude: true,
-            replace: true,
             templateUrl: 'shared/map/marker/marker_template.html',
             scope: {
+                gmapElement: '@',
+                gmapTarget: '=',
                 position: '=',
                 title: '@'
             },
-            controller: ['$scope', '$q', function ($scope, $q) {
-                $scope.controller = this;
-                this._deferred = $q.defer();
-                this.ready = function (callback) {
-                    this._deferred.promise.then(callback);
-                };
-            }],
-            require: '^^googleMap',
-            link: function (scope, element, attrs, controller) {
-                controller.ready(function (map) {
+            link: function (scope, element, attrs, controller, transcludeFn) {
+                // Create a new transclusion scope and add a marker promise to it.
+                var transclusionScope = scope.$new();
+                var transclusionTarget = element[0].querySelector('[transclude-target]');
+                transclusionScope[scope.gmapElement] = $q.defer();
+                transcludeFn(transclusionScope, function (clone) {
+                    angular.element(transclusionTarget).append(clone);
+                });
+
+                scope.gmapTarget.promise.then(function (map) {
+                    // Create a marker.
                     var marker = new google.maps.Marker({
                         position: scope.position,
                         map: map,
                         title: scope.title
                     });
 
-                    scope.controller._deferred.resolve(marker);
+                    transclusionScope[scope.gmapElement].resolve(marker);
                 });
             }
         };
