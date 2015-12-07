50169ca6586cd9689eef8a329a502aa1695b81de
Lars Woxberg <lars.woxberg@gmail.com>
2015-12-07 17:43:24 +0100

    Marker events extended with all available events.
    Now all provided events by google maps can be used on markers.

--- BusPlanner.Web/app/shared/map/marker/marker_event_directives.js
+++ BusPlanner.Web/app/shared/map/marker/marker_event_directives.js
@@ -1,19 +1,48 @@
 ﻿(function () {
     "use strict";
 
-    angular.module('map')
-    .directive('gmapMarkerClick', ['$parse', function ($parse) {
-        return {
-            restrict: 'A',
-            require: '^googleMapMarker',
-            link: function (scope, element, attrs, controller) {
-                var fn = $parse(attrs['gmapMarkerClick']);
-                controller.gmapElement.then(function (gmapElement) {
-                    gmapElement.element.addListener('click', function (e) {
-                        fn(scope, { event: e });
-                    });
-                });
-            }
-        };
-    }]);
+    var eventNames = {
+        'AnimationChanged': 'animation_changed',
+        'Click': 'click',
+        'ClickableChanged': 'clickable_changed',
+        'CursorChanged': 'cursor_changed',
+        'Dblclick': 'dblclick',
+        'Drag': 'drag',
+        'Dragend': 'dragend',
+        'DraggableChanged': 'draggable_changed',
+        'Dragstart': 'dragstart',
+        'FlatChanged': 'flat_changed',
+        'IconChanged': 'icon_changed',
+        'Mousedown': 'mousedown',
+        'Mouseout': 'mouseout',
+        'Mouseover': 'mouseover',
+        'Mouseup': 'mouseup',
+        'PositionChanged': 'position_changed',
+        'Rightclick': 'rightclick',
+        'ShapeChanged': 'shape_changed',
+        'TitleChanged': 'title_changed',
+        'VisibleChanged': 'visible_changed',
+        'ZindexChanged': 'zindex_changed'
+    };
+
+    Object.keys(eventNames).forEach(function (eventName) {
+        var directiveName = 'gmapMarker' + eventName;
+        angular.module('map')
+        .directive(directiveName, ['$parse', function ($parse) {
+            return {
+                restrict: 'A',
+                require: '^googleMapMarker',
+                compile: function(element, attrs) {
+                    var fn = $parse(attrs[directiveName]);
+                    return function link(scope, element, attrs, controller) {
+                        controller.gmapElement.then(function (gmapElement) {
+                            gmapElement.element.addListener(eventNames[eventName], function (e) {
+                                fn(scope, { event: e });
+                            });
+                        });
+                    }
+                }
+            };
+        }]);
+    });
 })();
